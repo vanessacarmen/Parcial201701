a171d543d8c43e9f655e200a5bb29e72c438212f
vanessacarmen <vanessacarmenq@gmail.com>
2017-05-09 20:54:24 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
   $servername = "us-cdbr-iron-east-03.cleardb.net";
-  $username = "b274da356beea4";
-  $password = "99e94da4";
-  $dbname = "ad_7900928d6d5bc55";
+  $username = "b74ba3320e82ec";
+  $password = "2d194843";
+  $dbname = "ad_bb6ddb2f34daf52";
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
